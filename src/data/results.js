80e87c9c6f5b4f5b6801143f2539b9a4c6c3d457
limonacio <limoestayferraris@gmail.com>
2026-06-19 00:32:17 -0300

    Merge branch 'worldcup-live' # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/data/results.js
+++ src/data/results.js
@@ -3,14 +3,14 @@ export const results = [
     home: 'Mexico',
     away: 'South Africa',
     homeGoals: 2,
-    awayGoals: 1,
+    awayGoals: 0,
   },
 
   {
     home: 'South Korea',
     away: 'Czech Republic',
-    homeGoals: 0,
-    awayGoals: 0,
+    homeGoals: 2,
+    awayGoals: 1,
   },
 
   {
@@ -18,5 +18,173 @@ export const results = [
     away: 'Bosnia',
     homeGoals: 1,
     awayGoals: 1,
+  },
+
+  {
+    home: 'United States',
+    away: 'Paraguay',
+    homeGoals: 4,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Qatar',
+    away: 'Switzerland',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Brazil',
+    away: 'Morocco',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Haiti',
+    away: 'Scotland',
+    homeGoals: 0,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Australia',
+    away: 'Turkey',
+    homeGoals: 2,
+    awayGoals: 0,
+  },
+
+  {
+    home: 'Germany',
+    away: 'Curacao',
+    homeGoals: 7,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Ivory Coast',
+    away: 'Ecuador',
+    homeGoals: 1,
+    awayGoals: 0,
+  },
+
+  {
+    home: 'Netherlands',
+    away: 'Japan',
+    homeGoals: 2,
+    awayGoals: 2,
+  },
+
+  {
+    home: 'Sweden',
+    away: 'Tunisia',
+    homeGoals: 5,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Spain',
+    away: 'Cape Verde',
+    homeGoals: 0,
+    awayGoals: 0,
+  },
+
+  {
+    home: 'Belgium',
+    away: 'Egypt',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Saudi Arabia',
+    away: 'Uruguay',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Iran',
+    away: 'New Zealand',
+    homeGoals: 2,
+    awayGoals: 2,
+  },
+
+  {
+    home: 'France',
+    away: 'Senegal',
+    homeGoals: 3,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Iraq',
+    away: 'Norway',
+    homeGoals: 1,
+    awayGoals: 4,
+  },
+
+  {
+    home: 'Argentina',
+    away: 'Algeria',
+    homeGoals: 3,
+    awayGoals: 0,
+  },
+
+  {
+    home: 'Austria',
+    away: 'Jordan',
+    homeGoals: 3,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Portugal',
+    away: 'DR Congo',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'England',
+    away: 'Croatia',
+    homeGoals: 4,
+    awayGoals: 2,
+  },
+
+  {
+    home: 'Ghana',
+    away: 'Panama',
+    homeGoals: 1,
+    awayGoals: 0,
+  },
+
+  {
+    home: 'Uzbekistan',
+    away: 'Colombia',
+    homeGoals: 1,
+    awayGoals: 3,
+  },
+
+  {
+    home: 'Czech Republic',
+    away: 'South Africa',
+    homeGoals: 1,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Switzerland',
+    away: 'Bosnia',
+    homeGoals: 4,
+    awayGoals: 1,
+  },
+
+  {
+    home: 'Canada',
+    away: 'Qatar',
+    homeGoals: 6,
+    awayGoals: 0,
   },
 ]
